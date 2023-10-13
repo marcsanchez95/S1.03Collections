f531ba6ea030944045ac4eba8cc9c123d163cce1
Wrennandsix <marcsmoreno@hotmail.com>
2023-10-13 21:25:47 +0200

    class restaurant equals modificado

--- S1.03Collections/src/n2ex01/Restaurant.java
+++ S1.03Collections/src/n2ex01/Restaurant.java
@@ -1,5 +1,7 @@
 package n2ex01;
 
+import java.util.Objects;
+
 public class Restaurant {
     private String name;
     private int score;
@@ -16,6 +18,20 @@ public class Restaurant {
     public int getScore() {
         return score;
     }
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, score);
+	}
+
+	@Override
+	public boolean equals(Object restaurant) {
+		if (this == restaurant)
+			return true;
+		if (restaurant == null || getClass() != restaurant.getClass())
+			return false;
+		Restaurant that =  (Restaurant) restaurant;
+		return score == that.score && Objects.equals(name, that.name);
+	}
 
     @Override
     public String toString() {
